c7c202b932d91e8901484c613e13d8723a199fe0
KoiTF2 <arencuevas08@gmail.com>
2025-09-03 21:27:52 +0800

    oninput -> listener

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function check(input, key) {
-    if (input.value == key)
-        input.classList.add("correct");
-    else
-        input.classList.remove("correct");
-}
+document.querySelectorAll("input").forEach((input) => {
+    input.addEventListener("input", () => {
+        if (input.value == input.getAttribute("key"))
+            input.classList.add("correct");
+        else
+            input.classList.remove("correct");
+    });
+});
